af98ce3fa09628f3cb2eb7e162a2aefb4c124105
Dominic <laskero1604@students.d211.org>
2018-12-04 14:33:45 -0600

    Added index of html, css, and js.
    Wednesday: Saved Eddies ball bouncing demo, and started to edit it in my own Ball demo folder.
    
    Thursday: added ball demo and started to try to get keypress.
    
    Friday: statrted  dead zone at bottom
    
    Monday/Tuesday: got the red box at the bottom to reset the position of the ball.

--- Dominic/dead zone at bottom/index.js
+++ Dominic/dead zone at bottom/index.js
@@ -9,7 +9,6 @@
 
 
  **/
-var z = 1;
 var c;
 var engine;
 var world;
@@ -23,19 +22,6 @@ window.addEventListener("load", () => {
     // Canvas reference
     c = document.getElementById("ca");
 
-    // Tracks mouse movement
-    c.addEventListener('dblclick', (event) => {//need to WORK ON THE COLLSION IF THE BALL GOES THROUGH THE BOTTOM OR FIND A WAY TO DETECT IT, REMOVE IT , THEN SPAWN ANOTHER BALL.
-        if(Matter.detector.){
-
-        }
-
-    }, false);
-
-
-
-
-
-
         // Matter.js setup
     engine = Matter.Engine.create();
     world = engine.world;
@@ -44,10 +30,55 @@ window.addEventListener("load", () => {
         max: { x: 1000, y: 800 }
     };
 
+    var ball = Matter.Bodies.circle(900, 100, 25, {//this is the pinball
+        density: 0.94,
+        friction: .04,
+        frictionAir: 0.00001,
+        restitution: .7,
+        render: {
+            fillStyle: '#F35e66',
+            strokeStyle: 'black',
+            lineWidth: 1
+        }
+    });
+    Matter.World.add(world, ball);
+
+    var wall = Matter.Bodies.rectangle(500, 825, 250, 50, {//this is the bottom red box
+        isStatic: true, //An immovable object
+        isSensor:true,
+        render: {
+            fillStyle: '#990000',
+            visible: true
+        }
+    });
+    Matter.World.add(world, wall);
+
+    var wall2 = Matter.Bodies.rectangle(400, 800, 50, 125, {//this is the left red box
+        isStatic: true, //An immovable object
+        isSensor:true,
+        render: {
+            fillStyle: '#990000',
+            visible: true
+        }
+    });
+    Matter.World.add(world, wall2);
+
+    var wall3 = Matter.Bodies.rectangle(600, 800, 50, 125, {//this is the right red box
+        isStatic: true, //An immovable object
+        isSensor:true,
+        render: {
+            fillStyle: '#990000',
+            visible: true
+        }
+    });
+    Matter.World.add(world, wall3);
+
+
     mouseConstraint = Matter.MouseConstraint.create(engine, {
         element: c,
         constraint: {
             render: {
+                fillStyle: '#990000',
                 visible: true
             },
             stiffness:0.8
@@ -58,23 +89,17 @@ window.addEventListener("load", () => {
 
     // Add all bodies to the world
     Matter.World.add(world, [
-
-        staticBox2(500, 780, 50, 50, "#000000"),
-        staticBox3(100, 500, 1000, 50, "#000000", 1, true),//turned box
-        staticBox3(199, 750, 450, 50, "#000000", 0, true),//left small box
-        staticBox3(800, 750, 450, 50, "#000000", 0, true),//right small box
-        staticBox3(500, 850, 1000, 50, "#990000", 0, false),//bottom box
-        staticBox3(400, 800, 100, 50, "#990000", 80.1, false),//80.1 is straight up and down
-        staticBox3(600, 800, 100, 50, "#990000", 80.1, false),//^^ left and right up and down box
-        ball(900, 100, 25),//ball size
+        staticBox2(500, 780, 50, 50, "#000000"),//right slide
+        staticBox3(100, 500, 1000, 50, "#000000", 1),//left slide
+        staticBox3(200, 900, 450, 350, "#000000", 0),//left small box
+        staticBox3(800, 900, 450, 350, "#000000", 0),//right small box
 
         // Window edges (top, bottom, left, right)
         border(500, -5, 1000, 10),//top
-        border(500, 1000, 1000, 10),//bottom
+        border(500, 850, 1000, 10),//bottom
         border(-5, 400, 10, 800),//left
-        border(1005, 400, 10, 800)//right
+        border(1005, 400, 10, 800),//right
     ]);
-
 
 // **!!REMEMBER!!** Set Renderer to match Canvas
     render = Matter.Render.create({
@@ -89,16 +114,30 @@ window.addEventListener("load", () => {
         }
     });
 
+    Matter.Events.on(engine, 'collisionStart', function(event) {
+        var pairs = event.pairs;
+        for (var i = 0, j = pairs.length; i != j; ++i) {
+            var pair = pairs[i];
+            if (pair.bodyA === ball && pair.bodyB === wall) {
+                Matter.Body.setPosition(ball, { x: 900, y: 100 });
+            } else if (pair.bodyA === ball && pair.bodyB === wall2) {
+                Matter.Body.setPosition(ball, { x: 900, y: 100 });
+            } else if (pair.bodyA === ball && pair.bodyB === wall3) {
+                Matter.Body.setPosition(ball, { x: 900, y: 100 });
+            }
+        }
+    });
 
     // Basic render
     Matter.Engine.run(engine);
     Matter.Render.run(render);
-
 });
 
 
 
 // ** Body Functions!! ** //
+
+
 
 function border(x, y, width, height) {
     return Matter.Bodies.rectangle(x, y, width, height, {
@@ -111,35 +150,6 @@ function border(x, y, width, height) {
     });
 }
 
-
-function ball(x, y, r) {
-    return Matter.Bodies.circle(x, y, r, {
-        density: 0.05,
-        friction: 0.0005, //affects friction so when it hits wall or ground slows down,
-        frictionAir: 0.003, // affects gravity speed
-        restitution: 1,
-        render: {
-            fillStyle: "#F35e66",
-            strokeStyle: "black",
-            lineWidth: 1
-        }
-    });
-}
-
-
-
-
-    function staticBox(x, y, width, height, colorHex) {
-        return Matter.Bodies.rectangle(x, y, width, height, {
-                isStatic: true,
-                angle: 0,
-                render: {
-                    fillStyle: colorHex,
-                    strokeStyle: "black",
-                    lineWidth: 1
-                }
-            });
-    }
     function staticBox2(x, y, width, height, colorHex) {
         return Matter.Bodies.rectangle(900, 500, 1000, height, {
             isStatic: true,
@@ -151,7 +161,7 @@ function ball(x, y, r) {
             }
         });
     }
-    function staticBox3(x, y, width, height, colorHex, angles, visible) {
+    function staticBox3(x, y, width, height, colorHex, angles) {
         return Matter.Bodies.rectangle(x, y, width, height, {
             isStatic: true,
 
